7e5842d7e291c33acde2eb234966b616634d18c6
Aditya1803 <adityatomar1803@gmail.com>
2022-08-03 01:20:19 +0530

    tried to approach selection sort differently

--- src/component/SortingAlgos/Quick.js
+++ src/component/SortingAlgos/Quick.js
@@ -5,6 +5,7 @@ export function quickSort(demoArr, dispatch, state, setPlay) {
   let collection = document.getElementsByClassName("scraper");
   demoArr = [...demoArr, Infinity];
   let low = 0,
+    // high = demoArr.length;
     high = demoArr.length - 1;
   quick(low, high);
 
@@ -13,45 +14,67 @@ export function quickSort(demoArr, dispatch, state, setPlay) {
       j = high;
 
     let pivot = demoArr[low];
-    // collection[low].style.backgroundColor = "blue";
-
+    setTimeout(() => {
+      collection[low].style.backgroundColor = "blue";
+    }, ++delay * state.speed * 2);
     while (i < j) {
       do {
-        // setTimeout(() => {
-        // collection[i - 1].style.backgroundColor = "pink";
-        // collection[i].style.backgroundColor = "yellow";
         i++;
-        // }, ++delay * state.speed * 4);
+        let f = i;
+        setTimeout(() => {
+          if (collection[f - 1].style.backgroundColor !== "blue") {
+            collection[f - 1].style.backgroundColor = "pink";
+          }
+          if (collection[f].style.backgroundColor !== "orange") {
+            collection[f].style.backgroundColor = "yellow";
+          }
+        }, ++delay * state.speed * 2);
       } while (demoArr[i] <= pivot);
       do {
-        // setTimeout(() => {
-        // collection[j - 1].style.backgroundColor = "pink";
-        // collection.style.backgroundColor = "red";
         j--;
-        // }, ++delay * state.speed * 4);
+        let f = j;
+        setTimeout(() => {
+          if (collection[f + 1].style.backgroundColor === "red") {
+            collection[f + 1].style.backgroundColor = "pink";
+          }
+          if (collection[f].style.backgroundColor !== "orange") {
+            collection[f].style.backgroundColor = "red";
+          }
+        }, ++delay * state.speed * 2);
       } while (demoArr[j] > pivot);
 
-      // setTimeout(() => {
       if (i < j) {
         let scapeGoat = demoArr[i];
         demoArr[i] = demoArr[j];
         demoArr[j] = scapeGoat;
+        let f = i,
+          k = j;
+
+        let anotherDemoArr = demoArr.slice();
+
+        setTimeout(() => {
+          collection[f].style.backgroundColor = "pink";
+          collection[k].style.backgroundColor = "pink";
+          dispatch({
+            type: ACTION_TYPES.NUMBERS,
+            payload: anotherDemoArr.slice(0, -1),
+          });
+        }, ++delay * state.speed * 2);
       }
-      // }, ++delay * state.speed * 4);
     }
 
-    let demo;
-    // setTimeout(() => {
     let scapeGoat = demoArr[low];
     demoArr[low] = demoArr[j];
     demoArr[j] = scapeGoat;
-    demo = demoArr.slice();
-    // }, ++delay * state.speed * 4);
+    let demo = demoArr.slice();
     setTimeout(() => {
       console.log("dispatched");
       dispatch({ type: ACTION_TYPES.NUMBERS, payload: demo.slice(0, -1) });
-      collection[j].style.backgroundColor = "blue";
-    }, ++delay * state.speed * 4);
+      collection[j].style.backgroundColor = "orange";
+      if (collection[j + 1].style.backgroundColor !== "orange") {
+        collection[j + 1].style.backgroundColor = "pink";
+      }
+    }, ++delay * state.speed * 2);
     return j;
   }
 
@@ -70,5 +93,5 @@ export function quickSort(demoArr, dispatch, state, setPlay) {
         collection[k].style.backgroundColor = "green";
       }, (k * state.speed) / 2);
     }
-  }, ++delay * state.speed * 4);
+  }, ++delay * state.speed * 2);
 }
--- src/component/SortingAlgos/Selection.js
+++ src/component/SortingAlgos/Selection.js
@@ -4,51 +4,80 @@ export default function selection(demoArr, dispatch, state, setPlay) {
   let collection = document.getElementsByClassName("scraper");
   let len = demoArr.length;
 
+  // for (let i = 0; i < len; i++) {
+  //   setTimeout(() => {
+  //     let smallest_index = i;
+
+  //     for (let j = i; j < len; j++) {
+  //       setTimeout(() => {
+  //         collection[smallest_index].style.backgroundColor = "blue";
+
+  //         if (j > i) {
+  //           if (collection[j - 1].style.backgroundColor !== "blue") {
+  //             collection[j - 1].style.backgroundColor = "pink";
+  //           }
+  //           collection[j].style.backgroundColor = "red";
+  //         }
+
+  //         if (demoArr[j] < demoArr[smallest_index]) {
+  //           collection[smallest_index].style.backgroundColor = "pink";
+  //           smallest_index = j;
+  //           collection[smallest_index].style.backgroundColor = "blue";
+  //         }
+  //         // }, (j * state.speed * 20) / 10);
+  //       }, j * state.speed * 2);
+  //     }
+
+  //     setTimeout(() => {
+  //       let scapegoat = demoArr[i];
+  //       demoArr[i] = demoArr[smallest_index];
+  //       demoArr[smallest_index] = scapegoat;
+  //       dispatch({ type: ACTION_TYPES.NUMBERS, payload: demoArr });
+  //       collection[smallest_index].style.backgroundColor = "pink";
+  //       collection[len - 1].style.backgroundColor = "pink";
+  //       collection[i].style.backgroundColor = "orange";
+  //     }, state.speed * len * 2);
+  //   }, i * state.speed * len * 2);
+  // }
+
+  // ---different code begins---
+  let delay = 0;
   for (let i = 0; i < len; i++) {
+    let smallest = i;
+
+    // ---small for creation of regular elements' background ---
+    let small = smallest;
+
     setTimeout(() => {
-      let smallest_index = i;
+      console.log(small);
+      collection[small].style.backgroundColor = "blue";
+    }, ++delay * state.speed * 4);
 
-      for (let j = i; j < len; j++) {
-        setTimeout(() => {
-          collection[smallest_index].style.backgroundColor = "blue";
-
-          if (j > i) {
-            if (collection[j - 1].style.backgroundColor !== "blue") {
-              collection[j - 1].style.backgroundColor = "pink";
-            }
-            collection[j].style.backgroundColor = "red";
-          }
-
-          if (demoArr[j] < demoArr[smallest_index]) {
-            collection[smallest_index].style.backgroundColor = "pink";
-            smallest_index = j;
-            collection[smallest_index].style.backgroundColor = "blue";
-          }
-          // }, (j * state.speed * 20) / 10);
-        }, j * state.speed * 2);
-      }
-
+    for (let j = i + 1; j < len; j++) {
       setTimeout(() => {
-        let scapegoat = demoArr[i];
-        demoArr[i] = demoArr[smallest_index];
-        demoArr[smallest_index] = scapegoat;
-        dispatch({ type: ACTION_TYPES.NUMBERS, payload: demoArr });
-        collection[smallest_index].style.backgroundColor = "pink";
-        collection[len - 1].style.backgroundColor = "pink";
-        collection[i].style.backgroundColor = "blue";
-      }, state.speed * len * 2);
-    }, i * state.speed * len * 2);
-  }
+        if (collection[j - 1].style.backgroundColor !== "blue") {
+          collection[j - 1].style.backgroundColor = "pink";
+        }
+        collection[j].style.backgroundColor = "red";
+      }, ++delay * state.speed * 4);
 
-  setTimeout(() => {
-    // collection[0].style.backgroundColor = "blue";
-    for (let i = 0; i < len; i++) {
-      setTimeout(() => {
-        collection[i].style.backgroundColor = "green";
-      }, (i * state.speed) / 2 + 500);
+      if (demoArr[j] < demoArr[smallest]) smallest = j;
     }
-    setTimeout(() => {
-      setPlay((prev) => !prev);
-    }, (len * state.speed) / 2 + 1000);
-  }, len * len * state.speed * 2 + 1000);
+
+    let demo = demoArr[i];
+    demoArr[i] = demoArr[smallest];
+    demoArr[smallest] = demo;
+  }
+  // --- different code ends ---
+
+  // setTimeout(() => {
+  //   for (let i = 0; i < len; i++) {
+  //     setTimeout(() => {
+  //       collection[i].style.backgroundColor = "green";
+  //     }, (i * state.speed) / 2 + 500);
+  //   }
+  //   setTimeout(() => {
+  //     setPlay((prev) => !prev);
+  //   }, (len * state.speed) / 2 + 1000);
+  // }, len * len * state.speed * 2 + 1000);
 }
